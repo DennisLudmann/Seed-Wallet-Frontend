32ab141ced2cd846c8ac25f63d7af4b9c4fc7887
Dennis Ludmann <luddilu@hotmail.com>
2022-12-22 21:32:20 +0100

    functions renderPage and renderSeedphrase added, now the seedphrase is displayed on page WIP

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let allWords = [];                          // BIP 39 - 2048words
-let seedPhrase = [];            
+let seedPhrase = [];
 let seedPhraseLenght = 12;
 let randomNumber;
 
@@ -7,6 +7,8 @@ let randomNumber;
 async function init() {
     await loadWords();
     generateSeedPhrase();
+    renderPage();
+    renderSeedphrase();
 }
 
 
@@ -14,7 +16,11 @@ function generateSeedPhrase() {             // picks 12 random words based on BI
     for (let i = 0; i < seedPhraseLenght; i++) {
         generateNumber();
         let word = allWords[randomNumber];
-        seedPhrase.push(word);
+        if (seedPhrase.includes(word)) {    // dont allow the seedphrase to contain the same word multiple times
+            generateSeedPhrase();
+        } else {
+            seedPhrase.push(word);
+        }
     }
     console.log(seedPhrase);
 }
@@ -26,6 +32,30 @@ async function loadWords() {
 }
 
 
+function renderPage(){
+    let contentEntryBuild = '';
+    contentEntryBuild += renderLanding(contentEntryBuild)
+    document.getElementById('container').innerHTML += contentEntryBuild;
+}
+
 function generateNumber() {                                 // generate random number (0-2047)
     randomNumber = Math.floor(Math.random() * 2048);
+}
+
+function renderSeedphrase() {
+    for (let i = 0; i < seedPhraseLenght; i++) {
+        let position = i +1;
+        document.getElementById('seedphrase').innerHTML += `<div id=${i} class="doors" href="#">${seedPhrase[i]} ${position}</div>`;
+    }
+}
+
+
+// Here are some builing blocks to generate HTML
+
+function renderLanding() {
+        return`
+        <h1 class="main__headline">Write Down Your Seed Phrase</h1>
+        <p p class="main__text" > This is your seed phrase.Write it down on a paper and keep it in a safe place.You'll be asked to
+        re - enter this phrase(in order) on the next step.</p>
+        <div id="seedphrase"></div>`
 }
