208c072d3ca4414cd4efe94c24abdf9e4e92c9b7
Dennis Ludmann <dennisludmann@gmail.com>
2023-01-02 13:26:37 +0100

    renaming HTML generated, next slide function WIP

--- scripts.js
+++ scripts.js
@@ -14,9 +14,9 @@ async function init() {
 
 function generateSeedPhrase() {             // picks 12 random words based on BIP 39
     for (let i = 0; i < seedPhraseLenght; i++) {
-        generateNumber();
+        generateNumber(2048);
         let word = allWords[randomNumber];
-        if (seedPhrase.includes(word)) {    // dont allow the seedphrase to contain the same word multiple times
+        if (seedPhrase.includes(word)) {    // dont allow the seedPhrase to contain the same word multiple times
             i--;
         } else {
             seedPhrase.push(word);
@@ -27,8 +27,8 @@ function generateSeedPhrase() {             // picks 12 random words based on BI
 
 
 function checkSeedphrase(input) {
-        let word = seedPhrase.findIndex((seedphrase__word) => seedphrase__word === input);
-    	console.log(word);
+    let word = seedPhrase.findIndex((seedphrase__word) => seedphrase__word === input);
+    console.log(word);
 }
 
 
@@ -38,37 +38,45 @@ async function loadWords() {
 }
 
 
-function renderPage(){
+function renderPage() {
     let contentEntryBuild = '';
-    contentEntryBuild += renderLanding(contentEntryBuild)
+    contentEntryBuild += landingHTML(contentEntryBuild)
     document.getElementById('container').innerHTML += contentEntryBuild;
 }
 
 
-function generateNumber() {                                 // generate random number (0-2047)
-    randomNumber = Math.floor(Math.random() * 2048);
+function generateNumber(number) {                                 // generate random number (0-2047 or 0-11 or 0-3)
+    randomNumber = Math.floor(Math.random() * number);
 }
 
 
 function renderSeedphrase() {
     for (let i = 0; i < seedPhraseLenght; i++) {
         let position = i + 1;
-        document.getElementById('seedphrase').innerHTML += `
-        <div id=${position} class="seedword" href="#">
-        <p class="seedphrase__number">${position}.</p>
-        <p class="seedphrase__word"> ${seedPhrase[i]}</p>
-         </div>
-         `;
+        document.getElementById('seedPhrase').innerHTML +=
+            seedphraseHTML(position, i);
     }
+}
+
+function nextSlide() {
+    let content = document.getElementById('container').innerHTML = "";
 }
 
 
 // Here are some builing blocks to generate HTML
 
-function renderLanding() {
-        return`
+function landingHTML() {
+    return `
         <h1 class="main__headline">Write Down Your Seed Phrase</h1>
         <p class="main__text" > This is your seed phrase.Write it down on a paper and keep it in a safe place.You'll be asked to
         re - enter this phrase(in order) on the next step.</p>
-        <div class="wrapper" id="seedphrase"></div>`
+        <div class="wrapper" id="seedPhrase"></div>`
+}
+
+function seedphraseHTML(position, i) {
+    return `
+        <div id=${position} class="seedword" href="#">
+        <p class="seedphrase__number">${position}.</p>
+        <p class="seedphrase__word"> ${seedPhrase[i]}</p>
+        </div>`
 }
